68f43d2faf9d3b4a582a050d823b2163dbda7ac0
Liberty Montano <code@libertysyarn.com>
2013-12-04 22:39:04 -0500

    step 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixednuts::Application.config.secret_token = 'cd7bebcd423e67ce80c1ec065318dd631e44d692bfc3a071c4e42c668a6a29eeedd3056c3a1133f927b8f6fa5acd215d1cf4ef2f7daa9d566bc80d4213cd89bf'
