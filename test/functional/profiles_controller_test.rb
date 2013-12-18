b64fecdcd7872d070abe8251a2faa6009ee71967
LibertysYarn <code@libertysyarn.com>
2013-12-18 17:17:57 -0500

    added scope profile pages

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:jason).profile_name
     assert_response :success
+    assert_template 'profiles/show'  
   end
+    
+    test "should render a 404 on profile not found" do
+        get :show, id: "doesn't exist"
+        assert_response :not_found
 
 end
+
+    test "that vaiables are assigned on successful profile viewing" do
+        get :show, id: users(:jason).profile_name
+        assert assigns(:user)
+        assert_not_empty assign(:statuses)
+    end    
+    
+    test "only shoes the correct usre's status"
+        get :show, id: users(:jason).profile_name
+        assigns(:statues).each do |status|
+            assert_equal users(:jason), status.user
+        end
+    end
