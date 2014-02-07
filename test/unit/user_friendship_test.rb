e882cd5e213770df015fade80ffdcfaec46af55e
LibertysYarn <code@libertysyarn.com>
2014-02-06 21:04:17 -0500

    finished state machine friend section

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -37,7 +37,34 @@ class UserFriendshipTest < ActiveSupport::TestCase
     end
   end
 
-   context "#accept!" do
+  context "#mutual_friendship" do
+    setup do
+      UserFriendship.request users(:jason), users(:jim)
+      @friendship1 = users(:jason).user_friendships.where(friend_id: users(:jim).id).first
+      @friendship2 = users(:jim).user_friendships.where(friend_id: users(:jason).id).first
+    end
+
+    should "correctly find the mutual friendship" do
+      assert_equal @friendship2, @friendship1.mutual_friendship
+    end
+  end
+
+  context "#accept_mutual_friendship!" do
+    setup do
+      UserFriendship.request users(:jason), users(:jim)
+    end
+
+    should "accept the mutual friendship" do
+      friendship1 = users(:jason).user_friendships.where(friend_id: users(:jim).id).first
+      friendship2 = users(:jim).user_friendships.where(friend_id: users(:jason).id).first
+
+      friendship1.accept_mutual_friendship!
+      friendship2.reload
+      assert_equal 'accepted', friendship2.state
+    end
+  end
+
+  context "#accept!" do
     setup do
       @user_friendship = UserFriendship.request users(:jason), users(:mike)
     end
@@ -50,14 +77,21 @@ class UserFriendshipTest < ActiveSupport::TestCase
     should "send an acceptance email" do
       assert_difference 'ActionMailer::Base.deliveries.size', 1 do
         @user_friendship.accept!
+      end
     end
-    
+
     should "include the friend in the list of friends" do
       @user_friendship.accept!
       users(:jason).friends.reload
       assert users(:jason).friends.include?(users(:mike))
     end
+
+    should "accept the mutual friendship" do
+      @user_friendship.accept!
+      assert_equal 'accepted', @user_friendship.mutual_friendship.state
+    end
   end
+
   context ".request" do
     should "create two user friendships" do
       assert_difference 'UserFriendship.count', 2 do
@@ -72,5 +106,48 @@ class UserFriendshipTest < ActiveSupport::TestCase
     end
   end
 
-end
+  context "#delete_mutual_friendship!" do
+    setup do
+      UserFriendship.request users(:jason), users(:jim)
+      @friendship1 = users(:jason).user_friendships.where(friend_id: users(:jim).id).first
+      @friendship2 = users(:jim).user_friendships.where(friend_id: users(:jason).id).first
+    end
+
+    should "delete the mutual friendship" do
+      assert_equal @friendship2, @friendship1.mutual_friendship
+      @friendship1.delete_mutual_friendship!
+      assert !UserFriendship.exists?(@friendship2.id)
+    end
+  end
+
+  context "on destroy" do
+    setup do
+      UserFriendship.request users(:jason), users(:jim)
+      @friendship1 = users(:jason).user_friendships.where(friend_id: users(:jim).id).first
+      @friendship2 = users(:jim).user_friendships.where(friend_id: users(:jason).id).first
+    end
+
+    should "delete the mutual friendship" do
+      @friendship1.destroy
+      assert !UserFriendship.exists?(@friendship2.id)
+    end
+  end
+
+  context "#block!" do
+    setup do
+      @user_friendship = UserFriendship.request users(:jason), users(:mike)
+    end
+
+    should "set the state to blocked" do
+      @user_friendship.block!
+      assert_equal 'blocked', @user_friendship.state
+      assert_equal 'blocked', @user_friendship.mutual_friendship.state
+    end
+
+    should "not allow new requests once blocked" do
+      @user_friendship.block!
+      uf = UserFriendship.request users(:jason), users(:mike)
+      assert !uf.save
+    end
+  end
 end
