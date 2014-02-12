45ac25a52042668fa0848cfb363974aa6a537aba
LibertysYarn <code@libertysyarn.com>
2014-02-12 16:42:44 -0500

    bug fix

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -1,10 +1,8 @@
 class UserFriendshipsController < ApplicationController
   before_filter :authenticate_user!
-  respond_to :html, :json
 
   def index
-    @user_friendships = UserFriendshipDecorator.decorate_collection(friendship_association.all)
-    respond_with @user_friendships
+    @user_friendships = current_user.user_friendships.all
   end
 
   def accept
@@ -13,16 +11,6 @@ class UserFriendshipsController < ApplicationController
       flash[:success] = "You are now friends with #{@user_friendship.friend.first_name}"
     else
       flash[:error] = "That friendship could not be accepted."
-    end
-    redirect_to user_friendships_path
-  end
-
-  def block
-    @user_friendship = current_user.user_friendships.find(params[:id])
-    if @user_friendship.block!
-      flash[:success] = "You have blocked #{@user_friendship.friend.first_name}."
-    else
-      flash[:error] = "That friendship could not be blocked."
     end
     redirect_to user_friendships_path
   end
@@ -43,21 +31,12 @@ class UserFriendshipsController < ApplicationController
     if params[:user_friendship] && params[:user_friendship].has_key?(:friend_id)
       @friend = User.where(profile_name: params[:user_friendship][:friend_id]).first
       @user_friendship = UserFriendship.request(current_user, @friend)
-      respond_to do |format|
-        if @user_friendship.new_record?
-          format.html do 
-            flash[:error] = "There was problem creating that friend request."
-            redirect_to profile_path(@friend)
-          end
-          format.json { render json: @user_friendship.to_json, status: :precondition_failed }
-        else
-          format.html do
-            flash[:success] = "Friend request sent."
-            redirect_to profile_path(@friend)
-          end
-          format.json { render json: @user_friendship.to_json }
-        end
+      if @user_friendship.new_record?
+        flash[:error] = "There was problem creating that friend request."
+      else
+        flash[:success] = "Friend request sent."
       end
+      redirect_to profile_path(@friend)
     else
       flash[:error] = "Friend required"
       redirect_to root_path
@@ -65,8 +44,8 @@ class UserFriendshipsController < ApplicationController
   end
 
   def edit
-    @friend = User.where(profile_name: params[:id]).first
-    @user_friendship = current_user.user_friendships.where(friend_id: @friend.id).first.decorate
+    @user_friendship = current_user.user_friendships.find(params[:id]).decorate
+    @friend = @user_friendship.friend
   end
 
   def destroy
@@ -75,21 +54,5 @@ class UserFriendshipsController < ApplicationController
       flash[:success] = "Friendship destroyed"
     end
     redirect_to user_friendships_path
-  end
-
-  private
-  def friendship_association
-    case params[:list]
-    when nil
-      current_user.user_friendships
-    when 'blocked'
-      current_user.blocked_user_friendships
-    when 'pending'
-      current_user.pending_user_friendships
-    when 'accepted'
-      current_user.accepted_user_friendships
-    when 'requested'
-      current_user.requested_user_friendships
-    end
   end
 end
